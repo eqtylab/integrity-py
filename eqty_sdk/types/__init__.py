f7938a8f30d5a418f0ceba147cd785286c57948c
Tyler <tyler@eqtylab.io>
2026-02-09 16:26:44 -0700

    move imports to init

--- eqty_sdk/types/__init__.py
+++ eqty_sdk/types/__init__.py
@@ -1,9 +1,9 @@
-from .cid import Cid
 from .declaration import Declaration
 from .did import Did
-from .entity import Entity
-from .manifest import Manifest
+from eqty_sdk._rust.entity import Entity
 from .signer import SIGNER_ALGORITHMS, Signer, set_active_signer
+from eqty_sdk._rust.cid import Cid
+from eqty_sdk._rust.manifest import Manifest
 
 __all__ = [
     "Cid",
