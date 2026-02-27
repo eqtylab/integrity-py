b80d722506ab29734fe26820f2ac8cabf1feea7c
Tyler <tyler@eqtylab.io>
2026-02-27 16:48:08 -0700

    sh compliant var

--- integration-tests/_install_and_run.sh
+++ integration-tests/_install_and_run.sh
@@ -9,22 +9,22 @@ EQTY_SDK_VERSION="${1:-latest}"
 TEST_SCRIPT="${2:-_basic_test.py}"
 
 INDEX_URL="http://$EQTY_PYPI_USER:$EQTY_PYPI_PASSWORD@eqty-pypi.westus2.cloudapp.azure.com/simple/"
-BREAK_SYSTEM_PACKAGES=()
+BREAK_SYSTEM_PACKAGES=""
 if pip install --help 2>&1 | grep -q -- "--break-system-packages"; then
-    BREAK_SYSTEM_PACKAGES=(--break-system-packages)
+    BREAK_SYSTEM_PACKAGES="--break-system-packages"
 fi
 
 if [ "$EQTY_SDK_VERSION" = "latest" ]; then
     pip install \
         --index-url "$INDEX_URL" \
         --trusted-host eqty-pypi.westus2.cloudapp.azure.com \
-        "${BREAK_SYSTEM_PACKAGES[@]}" \
+        ${BREAK_SYSTEM_PACKAGES:+$BREAK_SYSTEM_PACKAGES} \
         eqty_sdk
 else
     pip install \
         --index-url "$INDEX_URL" \
         --trusted-host eqty-pypi.westus2.cloudapp.azure.com \
-        "${BREAK_SYSTEM_PACKAGES[@]}" \
+        ${BREAK_SYSTEM_PACKAGES:+$BREAK_SYSTEM_PACKAGES} \
         "eqty_sdk==$EQTY_SDK_VERSION"
 fi
 
